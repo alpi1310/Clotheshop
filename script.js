82be79234ed46587faad34964924dd9dfa06baab
Alka Pal <alkapal1308@gmail.com>
2023-05-22 09:41:53 +0530

    commiting js file

---
+++ script.js
@@ -0,0 +1,16 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar')
+
+
+bar.addEventListener('click', () => {
+    console.log("clicked")
+    nav.classList.add('active')
+})
+
+
+close.addEventListener('click', () => {
+    nav.classList.remove('active')
+})
+
+
